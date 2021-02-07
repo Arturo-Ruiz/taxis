20f83b8472c3e185be24935b2b14c6959d096c14
Arturo <gregory09ruiz@gmail.com>
2021-02-07 16:59:45 -0400

    first commit

---
+++ precache-manifest.12d9b8d12d8d256f80f9063ecf7fb3a3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff01e5d2f8cd1c72926c78d770bccb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e1a66e8e9a5b88437b2",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "77c0a3e40af080cff509",
+    "url": "/static/js/2.c55e6cfc.chunk.js"
+  },
+  {
+    "revision": "7e1a66e8e9a5b88437b2",
+    "url": "/static/js/main.50a17756.chunk.js"
+  },
+  {
+    "revision": "d13d1bc2aee2b803d459",
+    "url": "/static/js/runtime~main.7b43e75d.js"
+  },
+  {
+    "revision": "43d0c11f4a6ccbff9224caf009333e62",
+    "url": "/static/media/logo.43d0c11f.png"
+  },
+  {
+    "revision": "1e54addd931c49000ca5bad578cb22f2",
+    "url": "/static/media/money1.1e54addd.jpg"
+  },
+  {
+    "revision": "2e3870f317fda369b5ff451c0ede340b",
+    "url": "/static/media/money2.2e3870f3.jpg"
+  },
+  {
+    "revision": "1d26e667581e3c5d2752e74e9d268390",
+    "url": "/static/media/money3.1d26e667.jpg"
+  },
+  {
+    "revision": "a6d4484afdad7a40df2f90d23b45f13f",
+    "url": "/static/media/sidemenu_logo.a6d4484a.jpg"
+  }
+]);
